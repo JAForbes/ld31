5a778ddfa607cfabd7ab8614e3cc38b7d1cd95ad
James <james.a.forbes@gmail.com>
2014-12-06 16:43:36 +1100

    Fleshing out ECS

--- components.js
+++ components.js
@@ -1,3 +1,17 @@
 E({
   Canvas: {can:can}
 })
+
+shipA = E({
+  Energy: { value: 3},
+  Health: { value: 6 },
+  Action: {value: 'idle'},
+  State: { value: null, history: [] },
+  Actions: { actions: [ 'idle', 'idleDamage', 'chargeDamage', 'shieldDamage', 'shield', 'absorb', 'fire', 'charge']},
+  Frame: { frame: null },
+  ClickActivated: {
+    '#fire .active' : { components: [ { Fire: {} } ] },
+    '#shield .active' : { components: [ { Shield: {} } ] }
+    '#charge .active' : { components: [ { Shield: {} } ] }
+  }
+})
